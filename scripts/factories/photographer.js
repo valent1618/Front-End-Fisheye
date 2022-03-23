ceb38f83a7ef7d9c8fadf7462b3ac241e87464c9
Valentin Lemaire <valentinlemaire1618@gmail.com>
2022-03-23 18:07:41 +0100

    add data to user card

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,27 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { name, city, country, tagline, price, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    const location = document.createElement("h3");
+    location.textContent = `${city}, ${country}`;
+    const slogan = document.createElement("p");
+    slogan.textContent = tagline;
+    const tjm = document.createElement("p");
+    tjm.textContent = `${price}€/jour`;
+    tjm.className = "price";
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(location);
+    article.appendChild(slogan);
+    article.appendChild(tjm);
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
