cc0e0c59d2268be1c49ee0da2cc44e40232e5b7a
Valentin Lemaire <valentinlemaire1618@gmail.com>
2022-03-29 15:31:00 +0200

    add link on card photographer

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,32 +1,62 @@
 function photographerFactory(data) {
-  const { name, city, country, tagline, price, portrait } = data;
+  const { name, id, city, country, tagline, price, portrait } = data;
 
   const picture = `assets/photographers/${portrait}`;
 
   function getUserCardDOM() {
+    // create article
     const article = document.createElement("article");
+
+    // create link
+    const link = document.createElement("a");
+    link.setAttribute("href", `pages/photographer.html?${id}`);
+    link.setAttribute("aria-labelledby", "name");
+
+    // create image
     const img = document.createElement("img");
     img.setAttribute("src", picture);
-    img.setAttribute("alt", `Photo de ${name}`);
+    img.setAttribute("alt", "");
+
+    // create h2 for the name
     const h2 = document.createElement("h2");
     h2.textContent = name;
+    h2.setAttribute("id", "name");
     h2.setAttribute("aria-label", "Nom et prénom");
+
+    // Put image and h2 into the link
+    link.appendChild(img);
+    link.appendChild(h2);
+
+    // create paragraph
+    const paragraph = document.createElement("div");
+
+    // create h3 for the location
     const location = document.createElement("h3");
     location.textContent = `${city}, ${country}`;
     location.setAttribute("aria-label", "Localisation");
+
+    // create p for the slogan
     const slogan = document.createElement("p");
     slogan.textContent = tagline;
     slogan.setAttribute("aria-label", "Slogan");
+
+    // create p for the price
     const tjm = document.createElement("p");
     tjm.textContent = `${price}€/jour`;
     tjm.className = "price";
     tjm.setAttribute("aria-label", "Prix par jour");
-    article.appendChild(img);
-    article.appendChild(h2);
-    article.appendChild(location);
-    article.appendChild(slogan);
-    article.appendChild(tjm);
+
+    // Put location, slogan and price into the paragraph
+    paragraph.appendChild(location);
+    paragraph.appendChild(slogan);
+    paragraph.appendChild(tjm);
+
+    // Put link and paragraph into the article
+    article.appendChild(link);
+    article.appendChild(paragraph);
+
     return article;
   }
+
   return { name, picture, getUserCardDOM };
 }
