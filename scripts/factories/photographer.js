62f930914f40efb433248d860344dc6963d206f2
Valentin Lemaire <valentinlemaire1618@gmail.com>
2022-03-29 19:43:44 +0200

    pattern for page photographer

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,44 +1,50 @@
 function photographerFactory(data) {
   const { name, id, city, country, tagline, price, portrait } = data;
 
-  const picture = `assets/photographers/${portrait}`;
+  // create article
+  const article = document.createElement("article");
 
+  // create image
+  const img = document.createElement("img");
+  img.setAttribute("src", `../assets/photographers/${portrait}`);
+  img.setAttribute("alt", `Photo de ${name}`);
+
+  // create paragraph
+  const paragraph = document.createElement("div");
+
+  // create p for the location
+  const location = document.createElement("p");
+  location.textContent = `${city}, ${country}`;
+  location.className = "location";
+  location.setAttribute("aria-label", "Localisation");
+
+  // create p for the slogan
+  const slogan = document.createElement("p");
+  slogan.textContent = tagline;
+  slogan.className = "slogan";
+  slogan.setAttribute("aria-label", "Slogan");
+
+  // create user card for index
   function getUserCardDOM() {
-    // create article
-    const article = document.createElement("article");
+    // add class to article
+    article.className = "user-card";
 
     // create link
     const link = document.createElement("a");
-    link.setAttribute("href", `pages/photographer.html?${id}`);
+    link.setAttribute("href", `pages/photographer.html?id=${id}`);
     link.setAttribute("aria-labelledby", "name");
 
-    // create image
-    const img = document.createElement("img");
-    img.setAttribute("src", picture);
-    img.setAttribute("alt", "");
+    // change source for index
+    img.setAttribute("src", `assets/photographers/${portrait}`);
 
     // create h2 for the name
     const h2 = document.createElement("h2");
     h2.textContent = name;
     h2.setAttribute("id", "name");
-    h2.setAttribute("aria-label", "Nom et prénom");
 
     // Put image and h2 into the link
     link.appendChild(img);
     link.appendChild(h2);
-
-    // create paragraph
-    const paragraph = document.createElement("div");
-
-    // create h3 for the location
-    const location = document.createElement("h3");
-    location.textContent = `${city}, ${country}`;
-    location.setAttribute("aria-label", "Localisation");
-
-    // create p for the slogan
-    const slogan = document.createElement("p");
-    slogan.textContent = tagline;
-    slogan.setAttribute("aria-label", "Slogan");
 
     // create p for the price
     const tjm = document.createElement("p");
@@ -58,5 +64,27 @@ function photographerFactory(data) {
     return article;
   }
 
-  return { name, picture, getUserCardDOM };
+  // create article for the header of page photographer
+  function getContactCard() {
+    // add aria-label
+    article.setAttribute("aria-label", `Information sur ${name}`);
+
+    // create h1 for the name
+    const h1 = document.createElement("h1");
+    h1.textContent = name;
+    h1.setAttribute("id", "name");
+    h1.setAttribute("aria-label", "Nom et prénom");
+
+    // Put location, slogan and price into the paragraph
+    paragraph.appendChild(location);
+    paragraph.appendChild(slogan);
+
+    // Put paragraph into the article
+    article.appendChild(h1);
+    article.appendChild(paragraph);
+
+    return article;
+  }
+
+  return { getUserCardDOM, getContactCard, img };
 }
