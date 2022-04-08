3060cb2eff16a7b1a3adbb503ce880491b54e13f
Valentin Lemaire <valentinlemaire1618@gmail.com>
2022-04-08 11:38:33 +0200

    generic script for modal

---
+++ scripts/utils/modal.js
@@ -0,0 +1,84 @@
+// Handle focus on elements and their children
+function recursiveUnfocusable(el) {
+  el.setAttribute("tabindex", "-1");
+  [...el.children].forEach(recursiveUnfocusable);
+}
+function recursiveFocusable(el) {
+  el.removeAttribute("tabindex");
+  [...el.children].forEach(recursiveFocusable);
+}
+// Handle the focus on header and main
+function handleFocus() {
+  const headerLink = document.querySelector(".header-link");
+  const main = document.getElementById("main");
+
+  if (main.getAttribute("tabindex")) {
+    headerLink.removeAttribute("tabindex");
+    recursiveFocusable(main);
+  } else {
+    headerLink.setAttribute("tabindex", "-1");
+    recursiveUnfocusable(main);
+  }
+}
+
+const body = document.querySelector("body");
+
+// Display modal
+function displayModal(thisModal) {
+  const modal = document.getElementById(thisModal);
+  const dialog = modal.querySelector("dialog");
+
+  modal.style.display = "flex";
+  dialog.setAttribute("open", "true");
+
+  // Remove scroll on body
+  body.style.overflow = "hidden";
+
+  // Remove focus of header and main
+  handleFocus();
+
+  // Focus an element when we display the modal
+  if (thisModal === "contact_modal") {
+    document.getElementById("first-name").focus({
+      preventScroll: true,
+    });
+  } else if (thisModal === "media_modal") {
+    document.querySelector(".arrow-right").focus();
+  }
+}
+
+// Close the modal
+function closeModal(thisModal) {
+  const modal = document.getElementById(thisModal);
+  const dialog = modal.querySelector("dialog");
+
+  modal.style.display = "none";
+  dialog.setAttribute("open", "false");
+
+  // Add scroll on body
+  body.style.overflow = "scroll";
+
+  // Remove attribute who block focus on header and main
+  handleFocus();
+
+  // Focus an element when we close the modal
+  if (thisModal === "contact_modal") {
+    document.querySelector(".contact_button").focus();
+  } else if (thisModal === "media_modal") {
+    document.querySelector(".media-link").focus();
+  }
+}
+
+// Key event
+// If modal is open, detect escape for close it
+const modals = document.querySelectorAll(".modal");
+
+document.addEventListener("keydown", (e) => {
+  modals.forEach((modal) => {
+    if (modal.children[0].getAttribute("open") === "true") {
+      if (e.code === "Escape") {
+        modal.querySelector(".close_button").click();
+      }
+    }
+  });
+});
