95432e00ee5e670c05def59b0a04a4dadce54e49
Valentin Lemaire <valentinlemaire1618@gmail.com>
2022-04-08 11:33:22 +0200

    change repo

---
+++ scripts/components/contactForm.js
@@ -0,0 +1,33 @@
+const dialog = document.querySelector(".dialog-contact");
+const form = document.querySelector(".contact-form");
+
+// Form handler
+form.addEventListener("submit", (e) => {
+  // Stop the classic action form
+  e.preventDefault();
+
+  // Log datas into the console
+  for (let i = 0; i < 4; i++) {
+    console.log(`${e.target[i].name} : ${e.target[i].value}`);
+  }
+
+  // Create the thanks message
+  const thanksMessage = document.createElement("p");
+  thanksMessage.className = "thanks-message";
+  thanksMessage.textContent = `Merci pour votre message ${e.target[0].value}`;
+
+  // Create button for close the modal after the thanks message
+  const closeButton = document.createElement("button");
+  closeButton.setAttribute("type", "button");
+  closeButton.setAttribute("onclick", "closeModal('contact_modal')");
+  closeButton.setAttribute("alt", "Ferme la modal");
+  closeButton.className = "button form_button";
+  closeButton.textContent = "Fermer";
+
+  // Remove the form
+  form.style.display = "none";
+
+  // Add the thanks message and the close button
+  dialog.appendChild(thanksMessage);
+  dialog.appendChild(closeButton);
+});
