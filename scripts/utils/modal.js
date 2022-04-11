9ca20ff2bc1783f79f10eaa9f755cbefe82bbd25
Valentin Lemaire <valentinlemaire1618@gmail.com>
2022-04-11 12:35:14 +0200

    prevent scroll when close mdoal

--- scripts/utils/modal.js
+++ scripts/utils/modal.js
@@ -1,32 +1,3 @@
-// Handle focus on elements and their children
-function recursiveUnfocusable(el) {
-  el.setAttribute("tabindex", "-1");
-  [...el.children].forEach(recursiveUnfocusable);
-}
-function recursiveFocusable(el) {
-  el.removeAttribute("tabindex");
-  [...el.children].forEach(recursiveFocusable);
-}
-// Handle focus on header and main
-// Set attribute aria-hidden
-function handleFocus() {
-  const banner = document.getElementById("banner");
-  const headerLink = document.querySelector(".header-link");
-  const main = document.getElementById("main");
-
-  if (main.getAttribute("tabindex")) {
-    banner.setAttribute("aria-hidden", "false");
-    main.setAttribute("aria-hidden", "false");
-    headerLink.removeAttribute("tabindex");
-    recursiveFocusable(main);
-  } else {
-    banner.setAttribute("aria-hidden", "true");
-    main.setAttribute("aria-hidden", "true");
-    headerLink.setAttribute("tabindex", "-1");
-    recursiveUnfocusable(main);
-  }
-}
-
 const body = document.querySelector("body");
 
 // Display modal
@@ -73,7 +44,9 @@ function closeModal(thisModal) {
   if (thisModal === "contact_modal") {
     document.querySelector(".contact_button").focus();
   } else if (thisModal === "media_modal") {
-    document.querySelector(".media-link").focus();
+    document.querySelector(".media-link").focus({
+      preventScroll: true,
+    });
   }
 }
 
