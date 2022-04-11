50e5e297c9ad5c9416060f88d6bb6cdb681a9f62
Valentin Lemaire <valentinlemaire1618@gmail.com>
2022-04-11 12:34:21 +0200

    generalize handleFocus

---
+++ scripts/utils/handleFocus.js
@@ -0,0 +1,28 @@
+// Handle focus on elements and their children
+function recursiveUnfocusable(el) {
+  el.setAttribute("tabindex", "-1");
+  [...el.children].forEach(recursiveUnfocusable);
+}
+function recursiveFocusable(el) {
+  el.removeAttribute("tabindex");
+  [...el.children].forEach(recursiveFocusable);
+}
+// Handle focus on header and main
+// Set attribute aria-hidden
+function handleFocus() {
+  const banner = document.getElementById("banner");
+  const headerLink = document.querySelector(".header-link");
+  const main = document.getElementById("main");
+
+  if (main.getAttribute("tabindex")) {
+    banner.setAttribute("aria-hidden", "false");
+    main.setAttribute("aria-hidden", "false");
+    headerLink.removeAttribute("tabindex");
+    recursiveFocusable(main);
+  } else {
+    banner.setAttribute("aria-hidden", "true");
+    main.setAttribute("aria-hidden", "true");
+    headerLink.setAttribute("tabindex", "-1");
+    recursiveUnfocusable(main);
+  }
+}
